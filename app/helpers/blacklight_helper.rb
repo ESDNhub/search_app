03858050433d5e2d8c8c05fcc1e6ee0ff6e18ea4
jmignault <john.mignault@gmail.com>
2016-06-27 15:12:53 -0400

    Censor name

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -2,6 +2,6 @@ module BlacklightHelper
   include Blacklight::BlacklightHelperBehavior
 
   def application_name
-    "Empire State Digital Network, Bitches"
+    "Empire State Digital Network"
   end
 end
