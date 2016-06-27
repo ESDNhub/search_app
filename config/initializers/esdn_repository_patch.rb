a9db3767307358b5525d9aa838b7e1be15e8a97d
jmignault <john.mignault@gmail.com>
2016-06-27 08:56:56 -0400

    Temporary backout of NYPL for demo

--- config/initializers/esdn_repository_patch.rb
+++ config/initializers/esdn_repository_patch.rb
@@ -3,6 +3,12 @@ module Dbla
     extend Deprecation
     self.deprecation_horizon = 'blacklight 6.0'
 
+    def find id, params = {}
+      puts "find_url: " + "#{url}/#{id}?api_key=#{api_key}"
+      data = get("#{url}/#{id}?api_key=#{api_key}")
+      Response.new(data, params,{})
+    end
+
     def search params = {}
       data = nil
       #TODO Move this into a SearchBuilder, add a generator
@@ -10,13 +16,13 @@ module Dbla
         q = "?api_key=#{api_key}&q=#{params['q']}"
         fq = []
         blacklight_config.facet_fields.each do |f|
-          # byebug
           # [fiendName, facetConfig]
           next unless f[0] =~ /^(sourceResource|provider|object|intermediateProvider|dataProvider|relation)/
           fqv = f[0]
           fqv = fqv + ':' + f[1].pin if f[1].pin
           fq << fqv
         end
+        # byebug
         q << "&facets=#{fq.join(',')}" unless fq.empty?
         if params.page
           q << "&page=#{params.page}"
@@ -28,8 +34,13 @@ module Dbla
           value = "\"#{value}\"" if value.index(' ')
           q << "&#{facet_field}=#{CGI::escape(value)}"
         end
-        q << '&provider="Empire+State+Digital+Network"+OR+"New+York+Public+Library"'
+        # byebug
+        # url_suffix = '&provider="Empire+State+Digital+Network"+OR+"New+York+Public+Library"'
+        url_suffix = '&provider="Empire+State+Digital+Network"'
+        # q.match(/(provider.name|collection.name)=/) { |m| url_suffix = '' }
+        q << url_suffix
         uri = URI.encode(url + q)
+        puts "search_url: " + url + q
         parsed_uri = URI.parse(uri)
         data = get(parsed_uri)
       end
