4e1e35bffd43c07997dfc84fe290ab59ad29d2cc
jmignault <john.mignault@gmail.com>
2016-06-30 15:45:19 -0400

    Further idiomize & simplify render_thumbnail

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,7 @@
 module ApplicationHelper
   def render_thumbnail(document, options)
     if document[:object].respond_to? :start_with?
-      if document[:object].start_with? 'http://'
-        img_path = document[:object]
-      else
-        img_path = 'not_found.png'
-      end
+      img_path = document[:object].start_with?("http:/") ? document[:object] : 'not_found.png' 
       image_tag(img_path, options.merge(alt: presenter(document).document_heading))
     end
   end
